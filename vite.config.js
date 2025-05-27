47eb82b431b3a168b109da8f0180f75eafdbb332
Syed Murtaza Mubarak <syedmurtazamubarak1999@gmail.com>
2025-05-27 19:27:16 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio-Project-2/', // Your GitHub repo name here
   plugins: [react()],
 })
